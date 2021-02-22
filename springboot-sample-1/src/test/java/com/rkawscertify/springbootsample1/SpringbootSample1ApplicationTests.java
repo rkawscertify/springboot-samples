52a0c4ec5ec0f915a0ddbca8c801eb3fb10faa9e
Rajesh Kumar <78507846+rkawscertify@users.noreply.github.com>
2021-02-22 22:23:23 +0530

    0.2

---
+++ springboot-sample-1/src/test/java/com/rkawscertify/springbootsample1/SpringbootSample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkawscertify.springbootsample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
